ab514d7465078fff83a2118a2743e62c7d6c7f3e
jason-girl <741425387@qq.com>
2021-04-07 17:54:57 +0800

    feat: 修改请求方法，添加学院方法

--- common/send_method.py
+++ common/send_method.py
@@ -19,7 +19,7 @@ class SendMethod:
 
     # 静态方式不需要实例化，使用方式：类名.静态方法
     @staticmethod
-    def send_method(method, url, params=None, data=None):
+    def send_method(method, url, data, params):
         if method == "get" or method == "delete":
             response = requests.request(method=method, url=url, params=params)
         elif method == "post" or method == "put":
@@ -46,5 +46,18 @@ if __name__ == '__main__':
     url = "http://127.0.0.1:8000/api/departments/"
     method = "get"
     params = {"$dep_id_list": "111,113,112"}
-    res = SendMethod.send_method(method, url, params)
+    res = SendMethod.send_method(method, url, None, params)
     print(SendMethod.format_response(res))
+    # method = "post"
+    # data = {
+    #     "data": [
+    #         {
+    #             "dep_id": "T033",
+    #             "dep_name": "T033",
+    #             "master_name": "T033",
+    #             "slogan": "jT033a"
+    #         }
+    #     ]
+    # }
+    # res = SendMethod.send_method(method, url, data)
+    # print(SendMethod.format_response(res))
--- interface/add_department.py
+++ interface/add_department.py
@@ -9,13 +9,13 @@ from common.get_keyword import GetKeyword
 
 
 class AddDepartment:
-    def __init__(self, url, method="post"):
+    def __init__(self, method="post"):
         self.url = "http://127.0.0.1:8000/api/departments/"
         self.method = method
 
     def add_dep(self, data):
         """ 请求新增学员接口，针对单接口测试"""
-        response = SendMethod.send_method(self.method, self.url, data)
+        response = SendMethod.send_method(self.method, self.url, data, None)
         return response
 
     def get_depid(self, data):
@@ -24,3 +24,17 @@ class AddDepartment:
         # 获取新增学员接口返回值中的dep_id
         dep_id = GetKeyword.get_value_by_keyword(response["create_success"], "dep_id")
         return dep_id
+
+
+if __name__ == '__main__':
+    data = {
+        "data": [
+            {
+                "dep_id": "133333",
+                "dep_name": "133333",
+                "master_name": "T033",
+                "slogan": "jT033a"
+            }
+        ]
+    }
+    print(AddDepartment().add_dep(data))
